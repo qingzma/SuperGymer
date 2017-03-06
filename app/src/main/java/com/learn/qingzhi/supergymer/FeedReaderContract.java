576921764b5c1892a60acdf9a5217be429543449
qingzhi <qingzma@gmail.com>
2017-03-06 19:48:06 +0000

    db

---
+++ app/src/main/java/com/learn/qingzhi/supergymer/FeedReaderContract.java
@@ -0,0 +1,18 @@
+package com.learn.qingzhi.supergymer;
+
+/**
+ * Created by qingzhi on 06/03/2017.
+ */
+
+public final class FeedReaderContract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // make the constructor private.
+    private FeedReaderContract() {}
+
+    /* Inner class that defines the table contents */
+    public static class FeedEntry implements BaseColumns {
+        public static final String TABLE_NAME = "entry";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_SUBTITLE = "subtitle";
+    }
+}
