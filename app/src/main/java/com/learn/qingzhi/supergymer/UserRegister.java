090ab40b8d80e26469b576b8c032ca967d719720
wangxieric <wangxieric@gmail.com>
2017-03-06 20:56:59 +0000

    ...

---
+++ app/src/main/java/com/learn/qingzhi/supergymer/UserRegister.java
@@ -0,0 +1,35 @@
+package com.learn.qingzhi.supergymer;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+/**
+ * Created by wangxi on 06/03/2017.
+ */
+
+public class UserRegister extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_register);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.menu_home:
+                                Toast.makeText(getApplicationContext(), "menu_home", Toast.LENGTH_SHORT).show();
+                            case R.id.menu_scan:
+                                Toast.makeText(getApplicationContext(),"menu_scan",Toast.LENGTH_SHORT).show();
+                            case R.id.menu_user:
+                                Toast.makeText(getApplicationContext(),"menu_user",Toast.LENGTH_SHORT).show();
+                        }
+                        return true;
+                    }
+                });
+    }
+}
