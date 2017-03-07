8b48245d8e69d43d34daa002a215fab782c36ec9
langsha <langsha814@gmail.com>
2017-03-07 20:00:47 +0000

    a little

---
+++ app/src/main/java/com/learn/qingzhi/supergymer/EquiementActivity.java
@@ -0,0 +1,62 @@
+package com.learn.qingzhi.supergymer;
+
+import android.content.Intent;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import com.learn.qingzhi.supergymer.dumbbell.Part_YalingActivity;
+
+
+
+public class EquiementActivity extends AppCompatActivity {
+    Button[] btnCategory = new Button[5];
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_equiement);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layout2);
+        layout.setOrientation(LinearLayout.VERTICAL);  //Can also be done in xml by android:orientation="vertical"
+        //Button[] btnCategory = new Button[5];
+        for (int i = 0; i < 5; i++) {
+            LinearLayout row = new LinearLayout(this);
+            row.setLayoutParams(new AppBarLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+
+            btnCategory[i] = new Button(this);
+            btnCategory[i].setLayoutParams(new AppBarLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+            btnCategory[i].setText("Button " + i);
+            btnCategory[i].setId(i);
+            btnCategory[i].setOnClickListener(new EquiementActivity.MyButtionListener());
+                row.addView(btnCategory[i]);
+                layout.addView(row);
+
+
+        }
+
+    }
+    class MyButtionListener implements View.OnClickListener{
+
+        public void onClick(View v){
+
+                Intent intent =new Intent();
+                intent.setClass(EquiementActivity.this,Part_YalingActivity.class);
+                EquiementActivity.this.startActivity(intent);
+
+        }
+    }
+
+ }
+
+
+
+
+
+
+
+
+
