dc3fe0dc8434554c9fc4a55777d854331b96470c
qingzhi <qingzma@gmail.com>
2017-03-07 18:21:27 +0000

    database initialise

--- app/src/main/java/com/learn/qingzhi/supergymer/AndroidSQLiteTutorialActivity.java
+++ app/src/main/java/com/learn/qingzhi/supergymer/AndroidSQLiteTutorialActivity.java
@@ -4,18 +4,12 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import db.Contact;
 import db.DBHandler;
-import db.DatabaseHandler;
-import db.DbSetup;
-import db.Equipment;
 import db.User;
 
 public class AndroidSQLiteTutorialActivity extends AppCompatActivity {
