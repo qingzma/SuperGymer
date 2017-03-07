7e37c53a25e7a36de6e324e39c94b5710037bd8b
wangxieric <wangxieric@gmail.com>
2017-03-07 13:37:46 +0000

    ...

--- app/src/main/java/com/learn/qingzhi/supergymer/UserLogin.java
+++ app/src/main/java/com/learn/qingzhi/supergymer/UserLogin.java
@@ -1,10 +1,12 @@
 package com.learn.qingzhi.supergymer;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -22,7 +24,7 @@ public class UserLogin extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_SIGNUP = 0;
 
-    @BindView(R.id.input_name) EditText _emailText;
+    @BindView(R.id.input_name) EditText _nameText;
     @BindView(R.id.input_password) EditText _passwordText;
     @BindView(R.id.btn_login) Button _loginButton;
     @BindView(R.id.link_signup) TextView _signupLink;
@@ -44,7 +46,7 @@ public class UserLogin extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Start the Signup activity
-                Intent intent = new Intent(getApplicationContext(), UserRegister.class);
+                Intent intent = new Intent(getApplicationContext(), UserSignUp.class);
                 startActivityForResult(intent, REQUEST_SIGNUP);
             }
         });
@@ -68,6 +70,71 @@ public class UserLogin extends AppCompatActivity {
     }
 
     public void login(){
+        Log.d(TAG,"Login");
+        if(!validate()){
+            onLoginFailed();
+            return;
+        }
 
+        _loginButton.setEnabled(false);
+        final ProgressDialog progressDialog = new ProgressDialog(UserLogin.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        String name = _nameText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        //implement the authentication here !!!
+
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // disable going back to the MainActivity
+        moveTaskToBack(true);
+    }
+
+    public void onLoginSuccess(){
+        _loginButton.setEnabled(true);
+
+    }
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+        _loginButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String name = _nameText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        if (name.isEmpty()) {
+            _nameText.setError("enter a valid email address");
+            valid = false;
+        } else {
+            _nameText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        return valid;
     }
 }
