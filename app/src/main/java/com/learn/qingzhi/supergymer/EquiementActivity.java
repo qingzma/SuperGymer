7c691e5d42d4671368dc5433e0c4521c8eaa17c4
wangxieric <wangxieric@gmail.com>
2017-03-08 11:15:47 +0000

    ...

--- app/src/main/java/com/learn/qingzhi/supergymer/EquiementActivity.java
+++ app/src/main/java/com/learn/qingzhi/supergymer/EquiementActivity.java
@@ -1,6 +1,7 @@
 package com.learn.qingzhi.supergymer;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
 import com.learn.qingzhi.supergymer.dumbbell.Part_YalingActivity;
 
 import java.util.List;
