bad9a981c7caa3fd9bbc3805ea1555effb4e7e00
langsha <langsha814@gmail.com>
2017-03-08 16:45:28 +0000

    a little

--- app/src/main/java/com/learn/qingzhi/supergymer/Main_workflow/ExerciseDetailActivity.java
+++ app/src/main/java/com/learn/qingzhi/supergymer/Main_workflow/ExerciseDetailActivity.java
@@ -2,13 +2,16 @@ package com.learn.qingzhi.supergymer.Main_workflow;
 
 import android.content.Intent;
 import android.os.CountDownTimer;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,10 +31,13 @@ import static android.support.v7.appcompat.R.id.text;
 public class ExerciseDetailActivity extends AppCompatActivity {
     private VideoView video1=null;
     private TextView Text1=null;
-    private ToggleButton togglebutton;
+    //private ToggleButton togglebutton;
+    private Button button=null;
+    private Chronometer time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.arm_yaling);
 
         DBHandler dbHandler = new DBHandler(this);
@@ -68,24 +74,59 @@ public class ExerciseDetailActivity extends AppCompatActivity {
                         return true;
                     }
                 });
+        button=(Button)findViewById(R.id.button);
+        button.setTag(1);
+        button.setText("Begin");
+        time=(Chronometer) findViewById(R.id.chronometer2);
 
+       /* time.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+
+            }
+        });*/
 
         Text1 = (TextView) findViewById(R.id.textView);
         Text1.setText(equip.get_introduction());
         video1 = (VideoView) findViewById(R.id.videoView);
-        togglebutton = (ToggleButton) findViewById(R.id.toggleButton);
-        togglebutton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
+        time.setFormat("Exercise time:%s");
+        time.setBase(SystemClock.elapsedRealtime());
 
-                if (togglebutton.isChecked()) {
-                    Toast.makeText(ExerciseDetailActivity.this, "begin", Toast.LENGTH_SHORT).show();
-                }
+        button.setOnClickListener(new View.OnClickListener() {
 
-                else {
-                    Toast.makeText(ExerciseDetailActivity.this, "end", Toast.LENGTH_SHORT).show();
+            @Override
+            public void onClick (View v) {
+                int status =(Integer) v.getTag();
+               // time.setBase(SystemClock.elapsedRealtime());
+
+                if(status == 1) {
+                    time.setBase(SystemClock.elapsedRealtime());
+                    time.start();
+                    button.setText("Pause");
+                    v.setTag(0); //pause
+                } else {
+                    button.setText("Begin");
+
+                    time.stop();
+                    //v.setTag(1); //pause
                 }
             }
+
         });
+        //togglebutton = (ToggleButton) findViewById(R.id.toggleButton);
+        //togglebutton.setOnClickListener(new View.OnClickListener(){
+         /*   public void onClick(View v) {
+
+                if (togglebutton.isChecked()) {
+                    time.start();
+                }
+
+                else if(togglebutton.isActivated()) {
+
+                    time.stop();
+                }
+            }
+        });*/
 
 
     }
